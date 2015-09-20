5e3ca1e799e0f4798d3fd99769865ed471e946e9
Andrew Barger <asbarg01@gmail.com>
2015-09-20 18:04:54 -0400

    Adding a touch command to mimic Bash

--- Powershell/Microsoft.Powershell_profile.ps1
+++ Powershell/Microsoft.Powershell_profile.ps1
@@ -1,4 +1,5 @@
-﻿#Vim shorties
+﻿#Vim Shorties
+#
 $VIMPATH = "E:\PROGRAMFILES\VIM\VIM74\"
 Set-Alias vim $VIMPATH\VIM.EXE
 Set-Alias gvim $VIMPATH\GVIM.EXE
@@ -13,9 +14,16 @@ Function editVimrc
 	vim E:\ProgramFiles\Vim\_vimrc
 }
 
+#End Vim Shorties
+
+
 #Chrome
+#
 $CHROMEPATH = "C:\PROGRA~2\GOOGLE\CHROME\APPLICATION"
 Set-Alias chrome $CHROMEPATH\chrome.exe
+
+#End Chrome
+
 
 Function editPSProfile
 {
@@ -24,21 +32,51 @@ Function editPSProfile
 
 $console = $host.UI.RawUI
 
+
+#Powershell UI
+#
+
 #buffer
+#
 $buffer = $console.BufferSize
 $buffer.Width = 165
 $buffer.Height = 65
 $console.BufferSize = $buffer
 
 #Window
+#
 $size = $console.WindowSize
 $size.Width = 165
 $size.height = 65
 $console.WindowSize = $size
 
 #colors
+#
 $console.ForegroundColor = "Black"
 $console.BackgroundColor = "Gray"
 
+#End Powershell UI
+
+
 #remove curl alias so cURL actually works
 remove-item alias:curl
+
+#Bash Command Replacements
+#
+
+Function touch
+{
+    #set encoding to utf8 explicitly - defaults to utf16
+    echo $null | out-file -encoding utf8 $args[0]
+}
+
+#End Bash Command Replacements
+
+
+
+
+
+
+
+
+
